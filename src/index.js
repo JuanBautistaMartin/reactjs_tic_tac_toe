f8f4ae9054495cac5a300356b7ba2ecda57854c5
juanBautistaMartin <juanbautista19@gmail.com>
2020-12-18 14:01:27 +0100

    Now the state and functionality is in the Game component, the parent of all the components

--- src/index.js
+++ src/index.js
@@ -14,54 +14,16 @@ function Square(props) {
 
 class Board extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      // Setting X as the default value
-      xIsNext: true,
-    };
-  }
-
-  handleClick(i) {
-    // Creating a copy of squares, Inmutability
-    const squares = this.state.squares.slice();
-
-    // Checking if someone has won the game or if a Square is already filled.
-    // Because if the Square is not filled squares[i] return null, that's becasue
-    // of the initialization of squares in the Board component's constructor
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-
-    this.setState({
-      squares: squares,
-      // Negate the xIsNext attribute of the state, flipping between X and O
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
   renderSquare(i) {
     return <Square 
-              value={ this.state.squares[i] } 
-              onClick={() => this.handleClick(i)}
+              value={ this.props.squares[i] } 
+              onClick={() => this.props.onClick(i)}
             />;
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -83,14 +45,63 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      // Setting X as the default value
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    // Creating a copy of squares, Inmutability
+    const squares = current.squares.slice();
+
+    // Checking if someone has won the game or if a Square is already filled.
+    // Because if the Square is not filled squares[i] return null, that's becasue
+    // of the initialization of squares in the Board component's constructor
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+
+    this.setState({
+      history: history.concat([{
+        squares: squares,
+      }]),
+      // Negate the xIsNext attribute of the state, flipping between X and O
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[history.length -1];
+    const winner = calculateWinner(current.squares);
+
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board 
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}/>
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
