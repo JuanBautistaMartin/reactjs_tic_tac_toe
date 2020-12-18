d0920e5707ad643e0cfba05ab31267ec645bdbb4
juanBautistaMartin <juanbautista19@gmail.com>
2020-12-18 13:24:27 +0100

    Implemented flipping between players, X and O

--- src/index.js
+++ src/index.js
@@ -18,13 +18,19 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      // Setting X as the default value
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
+    // Creating a copy of squares, Inmutability
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -35,7 +41,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
