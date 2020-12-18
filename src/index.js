7872670a5a05d219cd82b88e35f21d9631979060
juanBautista <juanbautista19@gmail.com>
2020-12-18 15:19:14 +0100

    Moves traveling, old moves

--- src/index.js
+++ src/index.js
@@ -54,11 +54,12 @@ class Game extends React.Component {
       }],
       // Setting X as the default value
       xIsNext: true,
+      stepNumber: 0,
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     // Creating a copy of squares, Inmutability
     const squares = current.squares.slice();
@@ -78,13 +79,33 @@ class Game extends React.Component {
       }]),
       // Negate the xIsNext attribute of the state, flipping between X and O
       xIsNext: !this.state.xIsNext,
+      stepNumber: history.length,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length -1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move 
+        ? 'Go to move #' + move
+        : 'Go to game start';
+
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
 
     let status;
     if (winner) {
@@ -102,7 +123,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
